4903ad2f3c7aa380d8bf198c2c7fcdf681e60357
isneha44 <isneha44@gmail.com>
2022-08-16 12:32:16 +0530

    handelling the chat room controller

--- src/controller/ChatRoomController.java
+++ src/controller/ChatRoomController.java
@@ -4,12 +4,25 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class ChatRoomController {
 
     public JFXTextField messageTxt;
     public VBox vbox;
     public AnchorPane chatRoomContext;
+
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket socket;
+    String userName;
+    private FileChooser fileChooser;
+    private File filePath;
 
     public void SendOnAction(ActionEvent actionEvent) {
     }
