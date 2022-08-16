519b458a6fdd93be45668b83ced70b87086da89e
isneha44 <isneha44@gmail.com>
2022-08-16 15:23:15 +0530

    client handeler class created

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -11,6 +13,17 @@ public class ClientHandler {
     private Socket socket;
     private BufferedReader reader;
     private PrintWriter writer;
+
+    public ClientHandler(Socket socket, ArrayList<ClientHandler> clients) {
+        try {
+            this.socket = socket;
+            this.clients = clients;
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
