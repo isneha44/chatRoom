20d7fa60734fcefbce3452505727ceb2c7236279
isneha44 <isneha44@gmail.com>
2022-08-16 12:21:43 +0530

    updated

--- src/controller/ChatRoomController.java
+++ src/controller/ChatRoomController.java
@@ -1,5 +1,14 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.VBox;
+
 public class ChatRoomController {
 
+    public JFXTextField messageTxt;
+    public VBox vbox;
+
+    public void SendOnAction(ActionEvent actionEvent) {
+    }
 }
