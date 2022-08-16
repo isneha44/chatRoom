6697d4aabe4bedf6e2b085dfd28f3a04bc14b582
isneha44 <isneha44@gmail.com>
2022-08-16 15:10:53 +0530

    navigation forum created

--- src/util/UiNavigateUtil.java
+++ src/util/UiNavigateUtil.java
@@ -1,4 +1,22 @@
 package util;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class UiNavigateUtil {
+
+    public static void navigationForm(AnchorPane anchorPane, String url) throws IOException {
+
+        anchorPane.getChildren().clear();
+        Parent parent = FXMLLoader.load(UiNavigateUtil.class.getResource("../view/" + url + ".fxml"));
+        anchorPane.getChildren().add(parent);
+    }
+
+
+
+
+    
 }
