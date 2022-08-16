e75f3e7a60e9f8253929f4bf98f46eeecb4410e0
isneha44 <isneha44@gmail.com>
2022-08-16 15:21:27 +0530

    properties added

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,6 +1,16 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ClientHandler {
+
+    private ArrayList<ClientHandler> clients;
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
 
 
 }
