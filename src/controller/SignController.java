3e54feb7ba498ad487c7fc9ca84c57e700b6d125
isneha44 <isneha44@gmail.com>
2022-08-16 14:51:37 +0530

    signIn fxml and controller formed

--- src/controller/SignController.java
+++ src/controller/SignController.java
@@ -1,5 +1,17 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class SignController {
     public static String username;
+    public AnchorPane signContex;
+    public JFXTextField txtUserName;
+
+    public void SignOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelOnAction(ActionEvent actionEvent) {
+    }
 }
