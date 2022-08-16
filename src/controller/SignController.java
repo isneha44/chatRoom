6bddc27aae097d13096d0cd98da9400a5eda40a4
isneha44 <isneha44@gmail.com>
2022-08-16 15:02:03 +0530

    Updated the chat room

--- src/controller/SignController.java
+++ src/controller/SignController.java
@@ -4,14 +4,27 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+
 public class SignController {
     public static String username;
     public AnchorPane signContex;
     public JFXTextField txtUserName;
 
-    public void SignOnAction(ActionEvent actionEvent) {
+    public void initialize() throws IOException {
+
+    }
+
+    public void SignOnAction(ActionEvent actionEvent) throws IOException {
+
+        if (!txtUserName.getText().isEmpty()) {
+            username = txtUserName.getText();
+            UiNavigateUtil.newUi(signContex, "ChatRoom");
+        }
     }
 
     public void cancelOnAction(ActionEvent actionEvent) {
+
+        UiNavigateUtil.closeForm(signContex);
     }
 }
