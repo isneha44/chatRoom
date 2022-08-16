e04557f5465afcccc68c39445a17b2a750bb8c0d
isneha44 <isneha44@gmail.com>
2022-08-16 15:12:46 +0530

    new Ui and Close Window Created

--- src/util/UiNavigateUtil.java
+++ src/util/UiNavigateUtil.java
@@ -2,7 +2,9 @@ package util;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -15,8 +17,19 @@ public class UiNavigateUtil {
         anchorPane.getChildren().add(parent);
     }
 
+    public static void newUi(AnchorPane anchorPane, String url) throws IOException {
+        Stage stage=new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(UiNavigateUtil.class.getResource("../view/"+url+".fxml"))));
+        stage.centerOnScreen();
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public static void closeForm(AnchorPane anchorPane){
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
+        stage.close();
+    }
 
 
 
-    
 }
